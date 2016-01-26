5874a211cd68ad80b3d33724154bdaef2c1253fa
Brian J Kolowitz <bkolowitz@gmail.com>
2016-01-25 21:46:57 -0500

    add example1 week4

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -2,3 +2,7 @@ $(function () {
   $('body p').append(' <strong>and Javascript!</strong>');
   $('body').append('<blockquote class="blockquote-reverse">Do work</blockquote>');
 });
+
+function showMessage(message) {
+  alert(message);
+}
