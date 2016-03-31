eaaf8e57edadfe7cba6d559ae75b12458b4ff905
Brian J Kolowitz <bkolowitz@gmail.com>
2016-03-31 12:53:54 -0400

    added sql2o for week 13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,13 @@
 import static spark.Spark.*;
 
 import routes.*;
+import week13.Model;
+import week13.Sql2oModel;
+import week13.Week13Routes;
+import java.util.UUID;
+import org.sql2o.Sql2o;
+import org.sql2o.converters.UUIDConverter;
+import org.sql2o.quirks.PostgresQuirks;
 
 public class Main {
 
@@ -13,7 +20,20 @@ public class Main {
     //Object r = new JavaGettingStarted();
     //Object r = new Week6Routes();
     //Object r = new Week7Routes();
-    Object r = new Student1ProjectRoutes();
+    //Object r = new Student1ProjectRoutes();
+    
+    Sql2o sql2o = new Sql2o(
+            System.getenv().get("DATABASE_URL"),
+            System.getenv().get("DATABASE_USER"), 
+            System.getenv().get("DATABASE_PASSWORD"), 
+            new PostgresQuirks() {
+        {
+            // make sure we use default UUID converter.
+            converters.put(UUID.class, new UUIDConverter());
+        }
+    });
+    
+    Object r = new Week13Routes(sql2o);
     
   }
 
